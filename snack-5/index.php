4c5618caf8b5da8ab3c830f501ac67574ebc86c7
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-06-22 15:07:04 +0200

    Used explode() to split a string at every "."

---
+++ snack-5/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+$sentence = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A officiis doloribus quibusdam ullam tempora omnis doloremque nulla dolor qui vitae fuga iusto accusamus, aut quae. Aperiam aspernatur sint, maiores nobis nesciunt qui dignissimos quod assumenda. Eligendi asperiores quisquam distinctio perspiciatis?";
+$newSentences = explode(".",$sentence);
+var_dump($newSentences);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 5</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
