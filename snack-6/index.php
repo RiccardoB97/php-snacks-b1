6ea1393a23f1af465cf7b675b5c242dfeb2b1997
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-06-23 11:05:57 +0200

    USed for loop in array of associative arrays

--- snack-6/index.php
+++ snack-6/index.php
@@ -1,4 +1,27 @@
 <?php 
+ 
+$db = [
+    'teachers' => [
+        [
+            'name' => 'Michele',
+            'lastname' => 'Papagni'
+        ],
+        [
+            'name' => 'Fabio',
+            'lastname' => 'Forghieri'
+        ]
+    ],
+    'pm' => [
+        [
+            'name' => 'Roberto',
+            'lastname' => 'Marazzini'
+        ],
+        [
+            'name' => 'Federico',
+            'lastname' => 'Pellegrini'
+        ]
+    ]
+];
 
 ?>
 
@@ -9,8 +32,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack 6</title>
+    <style>
+        .gray{
+            background-color:gray;
+        }
+        .green{
+            background-color:green;
+        }
+    </style>
 </head>
 <body>
-    
+    <?php
+        $keys = array_keys($db);
+        var_dump($keys);
+        for ($i=0; $i < count($keys) ; $i++) {  
+            $key = $keys[$i]; ?>
+                <div class="<?php echo $key == 'teachers' ? 'gray' : 'green' ?>">
+                    <?php 
+                        for ($j=0; $j < count($db[$key]); $j++) { 
+                            $user = $db[$key][$j]; ?>
+                            <p><?= $user['name'] . ' '. $user['lastname']?></p>
+
+                      <?php  }
+                    ?>
+                </div>
+      <?php }
+    ?>
+
+
+
+
 </body>
 </html>
